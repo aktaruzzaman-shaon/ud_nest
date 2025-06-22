71493253f1409c9b1e34284315c563d53dd09cbd
shaonfe <Shaon.fe@bulipetech.com>
2025-06-22 17:52:59 +0600

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './config/app.config';
+import { appConfigSchema } from './config/config.types';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({
+      load: [appConfig],
+      validationSchema: appConfigSchema,
+      validationOptions: {
+        // allowUnknown: false,
+        abortEarly: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
